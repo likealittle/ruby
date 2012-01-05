7b9135c79d580a8ad17bf3c58710e1b46a98903f
prasanna <myprasanna@gmail.com>
2012-01-05 00:22:24 -0800

    Add hackathon read me and tests.

---
+++ hackathon/test/file_required_check.rb
@@ -0,0 +1,67 @@
+# When requiring 10K files ruby slows down like shit.
+# Ruby before loading a file wants to make sure it's not already require'd.
+# To do that it loops through a variable called $" and checks if this file is require'd.
+
+# To understand the $" variable, do the following:
+# touch sample_file.rb
+# irb
+# $"
+# require "./sample_file.rb"
+# $"
+#
+# You will now see that sample_file has been added to the set of all required files.
+
+PATH = "./hackathon/test/gen"
+
+def write_file(path, contents)
+  fh = File.new(path, 'w')
+  fh.write(contents)
+  fh.close
+end
+
+def generate_same_file
+  main = ""
+  10000.times do |i|
+    f = "%05d" % 0
+    main += "require '#{PATH}/#{f}'\n"
+  end
+
+  write_file("#{PATH}/main.rb",  main)
+end
+
+def generate_diff_files
+  main = ""
+  10000.times do |i|
+    f = "%05d" % i
+    write_file("#{PATH}/#{f}.rb", "")
+    main += "require '#{PATH}/#{f}'\n"
+  end
+
+  write_file("#{PATH}/main.rb",  main)
+end
+
+def run
+  res = `./ruby #{PATH}/main.rb &2>1`
+  raise "Error: #{res}" if res != ""
+end
+
+`mkdir gen &2>1`
+
+generate_same_file
+
+start = Time.now
+run
+same_file = Time.now - start
+puts "Requiring the same file 10K times ran in #{same_file} seconds."
+
+generate_diff_files
+start = Time.now
+run
+diff_file = Time.now - start
+puts "Requiring 10K diff empty files ran in #{diff_file} seconds."
+
+if diff_file > 2*same_file
+  puts "FAILED: You have not successfully patched ruby yet. 10K file requires take too long."
+else
+  puts "PASSED."
+end
---
+++ hackathon/test/too_many_require_dirs.rb
@@ -0,0 +1,73 @@
+# When require 1K files from 1K diff directories, ruby slows down a lot.
+# This happens because, ruby has a variable called $: which stores the ordered
+# priority of directories to search for the required file. Ruby kernel currently
+# loops through this array and checks one by one.
+
+# If you fix this operation's complexity to O(1) or O(log(n)) by using a clever trie,
+# then this test will pass. Make sure the trie handles the priority ordering though.
+
+# To understand the $: variable, do the following:
+# mkdir sample_dir
+# touch sample_dir/sample_file.rb
+# irb
+# require "sample_file.rb" # fails with error.
+# $: << "sample_dir" # ask ruby to look at the dir.
+# require "sample_file.rb" # works.
+
+PATH = "./hackathon/test/gen"
+
+def write_file(path, contents)
+  fh = File.new(path, 'w')
+  fh.write(contents)
+  fh.close
+end
+
+def generate_same_dir
+  main = ""
+  1000.times do |i|
+    f = "%05d" % i
+    main += "require '#{PATH}/#{f}'\n"
+  end
+
+  write_file("#{PATH}/main.rb",  main)
+end
+
+def generate_diff_dirs
+  main = ""
+  1000.times do |i|
+    d = "d%05d" % i
+    f = "%05d" % i
+    Dir.mkdir(File.expand_path("#{PATH}/#{d}"), 0700) rescue nil
+    write_file("#{PATH}/#{d}/#{f}.rb", "")
+    main += "$: << '#{PATH}/#{d}'\n"
+    main += "require '#{f}'\n"
+  end
+
+  write_file("#{PATH}/main.rb",  main)
+end
+
+def run
+  res = `./ruby #{PATH}/main.rb &2>1`
+  raise "Error: #{res}" if res != ""
+end
+
+`mkdir gen &2>1`
+
+generate_same_dir
+
+start = Time.now
+run
+same_dir = Time.now - start
+puts "Requiring 1K files in the same directory ran in #{same_dir} seconds."
+
+generate_diff_dirs
+start = Time.now
+run
+diff_dirs = Time.now - start
+puts "Requiring 1K files from 1K diff require dirs ran in #{diff_dirs} seconds."
+
+if diff_dirs > 2*same_dir
+  puts "FAILED: You have not successfully patched ruby yet. 1K directory requires take too long."
+else
+  puts "PASSED."
+end
