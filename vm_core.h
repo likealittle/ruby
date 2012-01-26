a3288d60bd2c583653cb0d5e2e97915ad7cd051d
Nikhil Garg <nikhilgarg28@gmail.com>
2012-01-27 02:35:31 +0530

    Cleaned up white space etc in vm_core.h

--- vm_core.h
+++ vm_core.h
@@ -1,13 +1,13 @@
 /**********************************************************************
- * 
- *  vm_core.h -
- * 
- *  $Author$
- *  created at: 04/01/01 19:41:38 JST
- * 
- *  Copyright (C) 2004-2007 Koichi Sasada
- * 
- **********************************************************************/
+
+  vm_core.h -
+
+  $Author$
+  created at: 04/01/01 19:41:38 JST
+
+  Copyright (C) 2004-2007 Koichi Sasada
+
+**********************************************************************/
 
 #ifndef RUBY_VM_CORE_H
 #define RUBY_VM_CORE_H
@@ -123,147 +123,147 @@ typedef unsigned long rb_num_t;
 struct iseq_compile_data_ensure_node_stack;
 
 typedef struct rb_compile_option_struct {
-  int inline_const_cache;
-  int peephole_optimization;
-  int tailcall_optimization;
-  int specialized_instruction;
-  int operands_unification;
-  int instructions_unification;
-  int stack_caching;
-  int trace_instruction;
-  int debug_level;
+    int inline_const_cache;
+    int peephole_optimization;
+    int tailcall_optimization;
+    int specialized_instruction;
+    int operands_unification;
+    int instructions_unification;
+    int stack_caching;
+    int trace_instruction;
+    int debug_level;
 } rb_compile_option_t;
 
 struct iseq_inline_cache_entry {
-  VALUE ic_vmstat;
-  VALUE ic_class;
-  union {
-    VALUE value;
-    rb_method_entry_t *method;
-    long index;
-  } ic_value;
+    VALUE ic_vmstat;
+    VALUE ic_class;
+    union {
+	VALUE value;
+	rb_method_entry_t *method;
+	long index;
+    } ic_value;
 };
 
 #if 1
 #define GetCoreDataFromValue(obj, type, ptr) do { \
-ptr = (type*)DATA_PTR(obj); \
+    ptr = (type*)DATA_PTR(obj); \
 } while (0)
 #else
 #define GetCoreDataFromValue(obj, type, ptr) Data_Get_Struct(obj, type, ptr)
 #endif
 
 #define GetISeqPtr(obj, ptr) \
-GetCoreDataFromValue(obj, rb_iseq_t, ptr)
+  GetCoreDataFromValue(obj, rb_iseq_t, ptr)
 
 struct rb_iseq_struct;
 
 struct rb_iseq_struct {
-  /***************/
-  /* static data */
-  /***************/
-  
-  VALUE type;          /* instruction sequence type */
-  VALUE name;          /* String: iseq name */
-  VALUE filename;      /* file information where this sequence from */
-  VALUE filepath;      /* real file path or nil */
-  VALUE *iseq;         /* iseq (insn number and openrads) */
-  VALUE *iseq_encoded; /* encoded iseq */
-  unsigned long iseq_size;
-  VALUE mark_ary; /* Array: includes operands which should be GC marked */
-  VALUE coverage;     /* coverage array */
-  unsigned short line_no;
-  
-  /* insn info, must be freed */
-  struct iseq_insn_info_entry *insn_info_table;
-  size_t insn_info_size;
-  
-  ID *local_table;        /* must free */
-  int local_table_size;
-  
-  /* method, class frame: sizeof(vars) + 1, block frame: sizeof(vars) */
-  int local_size;
-  
-  struct iseq_inline_cache_entry *ic_entries;
-  int ic_size;
-  
-  /**
-   * argument information
-   *
-   *  def m(a1, a2, ..., aM,                    # mandatory
-   *        b1=(...), b2=(...), ..., bN=(...),  # optinal
-   *        *c,                                 # rest
-   *        d1, d2, ..., dO,                    # post
-   *        &e)                                 # block
-   * =>
-   *
-   *  argc           = M
-   *  arg_rest       = M+N+1 // or -1 if no rest arg
-   *  arg_opts       = N
-   *  arg_opts_tbl   = [ (N entries) ]
-   *  arg_post_len   = O // 0 if no post arguments
-   *  arg_post_start = M+N+2
-   *  arg_block      = M+N + 1 + O + 1 // -1 if no block arg
-   *  arg_simple     = 0 if not simple arguments.
-   *                 = 1 if no opt, rest, post, block.
-   *                 = 2 if ambiguos block parameter ({|a|}).
-   *  arg_size       = argument size.
-   */
-  
-  int argc;
-  int arg_simple;
-  int arg_rest;
-  int arg_block;
-  int arg_opts;
-  int arg_post_len;
-  int arg_post_start;
-  int arg_size;
-  VALUE *arg_opt_table;
-  
-  size_t stack_max; /* for stack overflow check */
-  
-  /* catch table */
-  struct iseq_catch_table_entry *catch_table;
-  int catch_table_size;
-  
-  /* for child iseq */
-  struct rb_iseq_struct *parent_iseq;
-  struct rb_iseq_struct *local_iseq;
-  
-  /****************/
-  /* dynamic data */
-  /****************/
-  
-  VALUE self;
-  VALUE orig;         /* non-NULL if its data have origin */
-  
-  /* block inlining */
-  /*
-   * NODE *node;
-   * void *special_block_builder;
-   * void *cached_special_block_builder;
-   * VALUE cached_special_block;
-   */
-  
-  /* klass/module nest information stack (cref) */
-  NODE *cref_stack;
-  VALUE klass;
-  
-  /* misc */
-  ID defined_method_id;   /* for define_method */
-  
-  /* used at compile time */
-  struct iseq_compile_data *compile_data;
+    /***************/
+    /* static data */
+    /***************/
+
+    VALUE type;          /* instruction sequence type */
+    VALUE name;          	 /* String: iseq name */
+    VALUE filename;      /* file information where this sequence from */
+    VALUE filepath;      /* real file path or nil */
+    VALUE *iseq;         /* iseq (insn number and openrads) */
+    VALUE *iseq_encoded; /* encoded iseq */
+    unsigned long iseq_size;
+    VALUE mark_ary; /* Array: includes operands which should be GC marked */
+    VALUE coverage;     /* coverage array */
+    unsigned short line_no;
+
+    /* insn info, must be freed */
+    struct iseq_insn_info_entry *insn_info_table;
+    size_t insn_info_size;
+
+    ID *local_table;        /* must free */
+    int local_table_size;
+
+    /* method, class frame: sizeof(vars) + 1, block frame: sizeof(vars) */
+    int local_size;
+
+    struct iseq_inline_cache_entry *ic_entries;
+    int ic_size;
+
+    /**
+     * argument information
+     *
+     *  def m(a1, a2, ..., aM,                    # mandatory
+     *        b1=(...), b2=(...), ..., bN=(...),  # optinal
+     *        *c,                                 # rest
+     *        d1, d2, ..., dO,                    # post
+     *        &e)                                 # block
+     * =>
+     *
+     *  argc           = M
+     *  arg_rest       = M+N+1 // or -1 if no rest arg
+     *  arg_opts       = N
+     *  arg_opts_tbl   = [ (N entries) ]
+     *  arg_post_len   = O // 0 if no post arguments
+     *  arg_post_start = M+N+2
+     *  arg_block      = M+N + 1 + O + 1 // -1 if no block arg
+     *  arg_simple     = 0 if not simple arguments.
+     *                 = 1 if no opt, rest, post, block.
+     *                 = 2 if ambiguos block parameter ({|a|}).
+     *  arg_size       = argument size.
+     */
+
+    int argc;
+    int arg_simple;
+    int arg_rest;
+    int arg_block;
+    int arg_opts;
+    int arg_post_len;
+    int arg_post_start;
+    int arg_size;
+    VALUE *arg_opt_table;
+
+    size_t stack_max; /* for stack overflow check */
+
+    /* catch table */
+    struct iseq_catch_table_entry *catch_table;
+    int catch_table_size;
+
+    /* for child iseq */
+    struct rb_iseq_struct *parent_iseq;
+    struct rb_iseq_struct *local_iseq;
+
+    /****************/
+    /* dynamic data */
+    /****************/
+
+    VALUE self;
+    VALUE orig;         /* non-NULL if its data have origin */
+
+    /* block inlining */
+    /*
+     * NODE *node;
+     * void *special_block_builder;
+     * void *cached_special_block_builder;
+     * VALUE cached_special_block;
+     */
+
+    /* klass/module nest information stack (cref) */
+    NODE *cref_stack;
+    VALUE klass;
+
+    /* misc */
+    ID defined_method_id;   /* for define_method */
+
+    /* used at compile time */
+    struct iseq_compile_data *compile_data;
 };
 
 enum ruby_special_exceptions {
-  ruby_error_reenter,
-  ruby_error_nomemory,
-  ruby_error_sysstack,
-  ruby_special_error_count
+    ruby_error_reenter,
+    ruby_error_nomemory,
+    ruby_error_sysstack,
+    ruby_special_error_count
 };
 
 #define GetVMPtr(obj, ptr) \
-GetCoreDataFromValue(obj, rb_vm_t, ptr)
+  GetCoreDataFromValue(obj, rb_vm_t, ptr)
 
 #if defined(ENABLE_VM_OBJSPACE) && ENABLE_VM_OBJSPACE
 struct rb_objspace;
@@ -271,74 +271,74 @@ void rb_objspace_free(struct rb_objspace *);
 #endif
 
 typedef struct rb_vm_struct {
-  VALUE self;
-  
-  rb_thread_lock_t global_vm_lock;
-  
-  struct rb_thread_struct *main_thread;
-  struct rb_thread_struct *running_thread;
-  
-  st_table *living_threads;
-  VALUE thgroup_default;
-  
-  int running;
-  int thread_abort_on_exception;
-  unsigned long trace_flag;
-  volatile int sleeper;
-  
-  /* object management */
-  VALUE mark_object_ary;
-  
-  VALUE special_exceptions[ruby_special_error_count];
-  
-  /* load */
-  VALUE top_self;
-  VALUE load_path;
-  VALUE loaded_features;
-  struct st_table *loaded_features_hash;
-  struct st_table *loading_table;
-  
-  /* signal */
-  struct {
-    VALUE cmd;
-    int safe;
-  } trap_list[RUBY_NSIG];
-  
-  /* hook */
-  rb_event_hook_t *event_hooks;
-  
-  int src_encoding_index;
-  
-  VALUE verbose, debug, progname;
-  VALUE coverages;
-  
-  struct unlinked_method_entry_list_entry *unlinked_method_entry_list;
-  
-  #if defined(ENABLE_VM_OBJSPACE) && ENABLE_VM_OBJSPACE
-  struct rb_objspace *objspace;
-  #endif
+    VALUE self;
+
+    rb_thread_lock_t global_vm_lock;
+
+    struct rb_thread_struct *main_thread;
+    struct rb_thread_struct *running_thread;
+
+    st_table *living_threads;
+    VALUE thgroup_default;
+
+    int running;
+    int thread_abort_on_exception;
+    unsigned long trace_flag;
+    volatile int sleeper;
+
+    /* object management */
+    VALUE mark_object_ary;
+
+    VALUE special_exceptions[ruby_special_error_count];
+
+    /* load */
+    VALUE top_self;
+    VALUE load_path;
+    VALUE loaded_features;
+    struct st_table *loaded_features_hash;
+    struct st_table *loading_table;
+
+    /* signal */
+    struct {
+      VALUE cmd;
+      int safe;
+    } trap_list[RUBY_NSIG];
+
+    /* hook */
+    rb_event_hook_t *event_hooks;
+
+    int src_encoding_index;
+
+    VALUE verbose, debug, progname;
+    VALUE coverages;
+
+    struct unlinked_method_entry_list_entry *unlinked_method_entry_list;
+
+#if defined(ENABLE_VM_OBJSPACE) && ENABLE_VM_OBJSPACE
+    struct rb_objspace *objspace;
+#endif
 } rb_vm_t;
 
 typedef struct {
-  VALUE *pc;          /* cfp[0] */
-  VALUE *sp;          /* cfp[1] */
-  VALUE *bp;          /* cfp[2] */
-  rb_iseq_t *iseq;        /* cfp[3] */
-  VALUE flag;         /* cfp[4] */
-  VALUE self;         /* cfp[5] / block[0] */
-  VALUE *lfp;         /* cfp[6] / block[1] */
-  VALUE *dfp;         /* cfp[7] / block[2] */
-  rb_iseq_t *block_iseq;  /* cfp[8] / block[3] */
-  VALUE proc;         /* cfp[9] / block[4] */
-  const rb_method_entry_t *me;/* cfp[10] */
+    VALUE *pc;          /* cfp[0] */
+    VALUE *sp;          /* cfp[1] */
+    VALUE *bp;          /* cfp[2] */
+    rb_iseq_t *iseq;        /* cfp[3] */
+    VALUE flag;         /* cfp[4] */
+    VALUE self;         /* cfp[5] / block[0] */
+    VALUE *lfp;         /* cfp[6] / block[1] */
+    VALUE *dfp;         /* cfp[7] / block[2] */
+    rb_iseq_t *block_iseq;  /* cfp[8] / block[3] */
+    VALUE proc;         /* cfp[9] / block[4] */
+    const rb_method_entry_t *me;/* cfp[10] */
 } rb_control_frame_t;
 
 typedef struct rb_block_struct {
-  VALUE self;         /* share with method frame if it's only block */
-  VALUE *lfp;         /* share with method frame if it's only block */
-  VALUE *dfp;         /* share with method frame if it's only block */
-  rb_iseq_t *iseq;
-  VALUE proc;
+    VALUE self;         /* share with method frame if it's only block */
+    VALUE *lfp;         /* share with method frame if it's only block */
+    VALUE *dfp;         /* share with method frame if it's only block */
+    rb_iseq_t *iseq;
+    VALUE proc;
 } rb_block_t;
 
 #define GetThreadPtr(obj, ptr) \
